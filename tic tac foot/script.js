3eb2a6c6719fd5189ee805cae602fab2f11a14f2
Mr Burns (4 real) <60815639+themrburns@users.noreply.github.com>
2020-05-11 14:56:51 +0300

    Add files via upload

---
+++ tic tac foot/script.js
@@ -0,0 +1,83 @@
+var move = 1;
+
+function zero() {
+  if (1 == move) {
+    document.getElementById('zero').innerHTML = "X"
+    move++;
+  } else {
+    move--;
+    return;
+  }
+}
+function one() {
+  if (1 == move) {
+    document.getElementById('one').innerHTML = "X"
+    move++;
+  } else {
+    move--;
+    return;
+  }
+}
+function two() {
+  if (1 == move) {
+    document.getElementById('two').innerHTML = "X"
+    move++;
+  } else {
+    move--;
+    return;
+  }
+}
+function three() {
+  if (1 == move) {
+    document.getElementById('three').innerHTML = "X"
+    move++;
+  } else {
+    move--;
+    return;
+  }
+}
+function four() {
+  if (1 == move) {
+    document.getElementById('four').innerHTML = "X"
+    move++;
+  } else {
+    move--;
+    return;
+  }
+}
+function five() {
+  if (1 == move) {
+    document.getElementById('five').innerHTML = "X"
+    move++;
+  } else {
+    move--;
+    return;
+  }
+}
+function six() {
+  if (1 == move) {
+    document.getElementById('six').innerHTML = "X"
+    move++;
+  } else {
+    move--;
+    return;
+  }
+}
+function seven() {
+  if (1 == move) {
+    document.getElementById('seven').innerHTML = "X"
+    move++;
+  } else {
+    move--;
+    return;
+  }
+}
+function eight() {
+  if (1 == move) {
+    document.getElementById('eight').innerHTML = "X"
+    move++;
+  } else {
+    move--;
+    return;
+  }
+}
